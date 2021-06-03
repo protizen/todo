1d4e3e74dcad85fe505c27c3a09f53a20ad0d0e4
Yoseph <49052289+protizen@users.noreply.github.com>
2021-06-03 20:45:43 +0900

    Update router.js

--- router.js
+++ router.js
@@ -2,7 +2,7 @@ const Phone = {
 			template: `
 						<table border="1" cellspacing="0" style="background:#bfcfff;">
 							<tr><th>이름</th><th>전화번호</th></tr>
-							<tr><td>전진영</td><td>010-1111-2222</td></tr>
+							<tr><td>유관순</td><td>010-1111-2222</td></tr>
 							<tr><td>홍길동</td><td>010-1111-3333</td></tr>
 							<tr><td>이순신</td><td>010-1111-7777</td></tr>
 						</table>						
@@ -12,7 +12,7 @@ const Mail = {
 			template: `
 						<table border="1" cellspacing="0" style="background:#b9ff73;">
 							<tr><th>이름</th><th>이메일</th></tr>
-							<tr><td>전진영</td><td>jeon@ssabu.net</td></tr>
+							<tr><td>유관순</td><td>jeon@ssabu.net</td></tr>
 							<tr><td>홍길동</td><td>hong@ssabu.net</td></tr>
 							<tr><td>이순신</td><td>lee@ssabu.net</td></tr>
 						</table>
